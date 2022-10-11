17603af392fb651dfef4308d70868c8b2b823d7e
mahkaz <marcus.tan.jun.wen@gmail.com>
2022-10-11 12:42:24 +0800

    add speed label and clean up code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const reset_btn = document.querySelector("#reset");
 const pause_btn = document.querySelector("#pause");
 const start_btn = document.querySelector("#start");
 const start_rand_btn = document.querySelector("#start-random");
-const speed_slider = document.querySelector(".speed-slider input")
+const speed_slider = document.querySelector("#speed-slider")
 const ctx = canvas.getContext("2d")
 
 
@@ -12,94 +12,20 @@ game.gameSetUp()
 game.fillArray();
 
 
-let MAX_SIMULATION_SPEED = 1700; // Higher value is slower, set <input> max at index.html.
-let SIMULATION_SPEED = MAX_SIMULATION_SPEED;
+let MAX_SIMULATION_TIMEOUT = 1100; // Higher value is slower, set <input> max at index.html.
+let simulation_timeout = MAX_SIMULATION_TIMEOUT - speed_slider.value;
+let gen_per_sec = 1/(simulation_timeout/1000)
+updateElementValue("speed-label","Speed: "+ gen_per_sec.toFixed(2) +" gen/sec")
+
 let isPlaying = false;
 let interval_id = 0;
 
-// wrapper for play/pause simulation
-function playSimulation() {
-  interval_id = window.setInterval(() => {
-    if (isPlaying) {
-      game.runGame();
-    }
-  }, SIMULATION_SPEED)
+function program() {
+  draw();
+  controls();
 }
 
 window.onload = () => {
-
-  // drawing cells
-  canvas.addEventListener('mousedown', (e) => {
-    x = e.offsetX;
-    y = e.offsetY;
-    game.makeAlive(x,y);
-    isDrawing = true;
-  });
-
-  canvas.addEventListener('mousemove', (e) => {
-    if (isDrawing) {
-      x = e.offsetX;
-      y = e.offsetY;
-      game.makeAlive(x,y);
-    }
-  });
-
-  document.addEventListener('mouseup', (e) => {
-    if (isDrawing) {
-      x = 0;
-      y = 0;
-      isDrawing = false;
-    }
-  });
-
-  // start simulation
-  start_btn.addEventListener("click", () => {
-    isPlaying = true;
-    if (interval_id)
-      clearInterval(interval_id);
-    playSimulation()
-  })
-
-  // default, start random
-  // to-do: add percentage of cells to randomise, 
-  // e.g. 80% will be either 1 or 0, 20% will be 0
-  start_rand_btn.addEventListener("click", () => {
-    isPlaying = true;
-    game.arrayRandomize();
-    game.fillArray();
-
-    playSimulation()
-  })
-
-  // pause board
-  pause_btn.addEventListener("click", () => {
-    if (isPlaying) {
-      isPlaying = false;
-    }
-    clearInterval(interval_id);
-  })
-  // pause_btn.addEventListener("click", () => {
-  //     reset_btn.style.backgroundColor = "#9ebe78d7"
-  // })
-
-
-  // reset board
-  reset_btn.addEventListener("click", () => {
-    if (isPlaying) {
-      isPlaying = false;
-    }
-    game.gameSetUp();
-    game.fillArray();
-  })
-  // reset_btn.addEventListener("mousedown", () => {
-  //     reset_btn.style.backgroundColor = "#9ebe78d7"
-  // })
-  // reset_btn.addEventListener("mouseup", () => {
-  //     reset_btn.style.backgroundColor = "#ffffff99"
-  // })
-  speed_slider.addEventListener("change", (event) => {
-    SIMULATION_SPEED = MAX_SIMULATION_SPEED - event.target.value;
-    clearInterval(interval_id);
-    playSimulation();
-  })
+  draw();
+  controls();
 }
